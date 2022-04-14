2785d0be2755f36430a08f5c18ba7d38993035ae
Mariana Murillo <mariana@primarix.com>
2022-04-14 14:30:09 -0600

    Solucion de Tarea 1, b95487

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, {  }  from 'react';
+
+export default function myApp() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Mariana Murillo Quintana - B95487
+    </h1>
+  )
+}
