59dce7f7deb1e41d7983f3de7a635ab1f404bc25
marianam4501 <56421784+marianam4501@users.noreply.github.com>
2022-04-30 11:27:30 -0600

    Cambio de proyecto 2

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, {  }  from 'react';
-
-export default function myApp() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Mariana Murillo Quintana - B95487
-    </h1>
-  )
-}
